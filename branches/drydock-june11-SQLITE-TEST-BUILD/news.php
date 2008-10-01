c11a7393687107f47d7ee29b7fda5129f33d34f5
TaQSuQs <TaQSuQs@39816690-d32b-0410-a0ec-457b0bb6e68a>
2008-10-01 07:44:16 +0000

    Changed drydock link/info in footer for templates and news page

--- branches/drydock-june11-SQLITE-TEST-BUILD/news.php
+++ branches/drydock-june11-SQLITE-TEST-BUILD/news.php
@@ -66,4 +66,4 @@
 
 <div align="center">- <a href="http://thorn.pichan.org/" target="blank">Thorn</a> +
 <a href="http://wakaba.c3.cx/s/web/wakaba_kareha.html" target="_blank">Wakaba</a> +
-<a href="http://code.573chan.org/" target="_blank">drydock <?php echo THversion ?></a> -</div>
+<a href="http://code.573chan.org/" target="_blank">drydock</a> -</div>
