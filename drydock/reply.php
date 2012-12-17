e6a687a48ca21cb905465be532ca9ea980c049d2
taqsuqs <TaQSuQs@gmail.com>
2012-12-17 01:29:25 +0000

    Oh, so like, I finally figured out why the blacklist wasn't working on kchan

--- drydock/reply.php
+++ drydock/reply.php
@@ -64,9 +64,13 @@ if ($mod == false) {
     @include(THpath . '/unlinked/blacklist.php');
     //You could use any website, or even CENSORED or some other text.  We picked GameFAQs.
     if (count($spamblacklist) > 0) {
+/*
         $_POST['body'] = str_replace($spamblacklist, "xxxxx", $_POST['body']);
         $_POST['link'] = str_replace($spamblacklist, "xxxxx", $_POST['link']);
         $_POST['name'] = str_replace($spamblacklist, "xxxxx", $_POST['name']);
+*/
+	//The logic on this was entirely wrong.  If you want to filter out certain things, set up wordfilters.  This now (as of 12/16/12), stops the post.
+	THdie("One of the phrases included in your post is not allowed.");
     }
 
     // The "email" field will have a big "IF YOU ARE HUMAN DO NOT FILL THIS IN" next to it.  Bots might get tricked.
